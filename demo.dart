cb3f8a0e57674c4c3b39080975408ac12f524c93
BlueTech Solutions <91244998+vinfotech1996@users.noreply.github.com>
2022-05-26 08:38:06 +0530

    Create demo.dart

---
+++ demo.dart
@@ -0,0 +1,15 @@
+//=======================Simple text demo.========================
+return const Text(
+      'Simple text demo.',
+      // Optional params to try:
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        color: Colors.blue,
+        fontSize: 32.0,
+        fontStyle: FontStyle.italic,
+        decoration: TextDecoration.underline,
+      ),
+      // Or:
+      // style: Theme.of(context).textTheme.xxx
+    );
+//==============================
